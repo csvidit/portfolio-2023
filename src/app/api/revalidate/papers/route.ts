e26204c7da4967dde687898117829e0fec173f65
Vidit Khandelwal <viditk17@gmail.com>
2023-10-11 18:36:07 -0400

    added revalidation routes for server-rendered pages RSC updates

---
+++ src/app/api/revalidate/papers/route.ts
@@ -0,0 +1,15 @@
+import { revalidatePath, revalidateTag } from "next/cache";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const secret = request.headers.get("secret");
+
+  if (secret !== process.env.REVALIDATE_SECRET) {
+    return Response.json({ message: "Invalid secret" }, { status: 401 });
+  }
+
+  revalidatePath("/papers");
+  revalidatePath("/papers/[slug]");
+
+  return Response.json({ revalidated: true, now: Date.now() })
+}
--- src/components/ContactForm/MessageItem.tsx
+++ src/components/ContactForm/MessageItem.tsx
@@ -4,7 +4,7 @@ const MessageItem = (props: { type: string; children: React.ReactNode, id: numbe
 
   return (
     <AnimatePresence>
-      <motion.div id={props.id.toString()} className={`relative rounded-2xl ${props.type == "user" ? "bg-blue-600 self-end" : "bg-neutral-800"} text-neutral-100 w-fit`}>
+      <motion.div id={props.id.toString()} className={`relative rounded-2xl ${props.type == "user" ? "bg-blue-600 self-end rounded-r-md" : "bg-neutral-800 rounded-l-lg"} text-neutral-100 w-fit`}>
         <motion.div className="px-4 py-2">{props.children}</motion.div>
       </motion.div>
     </AnimatePresence>
