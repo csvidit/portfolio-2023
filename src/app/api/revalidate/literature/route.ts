8989868ff634f42245e562cc341912a14b8882a9
Vidit Khandelwal <viditk17@gmail.com>
2023-10-11 18:38:54 -0400

    repaced Response with NextResponse

--- src/app/api/revalidate/literature/route.ts
+++ src/app/api/revalidate/literature/route.ts
@@ -1,15 +1,15 @@
 import { revalidatePath, revalidateTag } from "next/cache";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const secret = request.headers.get("secret");
 
   if (secret !== process.env.REVALIDATE_SECRET) {
-    return Response.json({ message: "Invalid secret" }, { status: 401 });
+    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
   revalidatePath("/literature");
   revalidatePath("/literature/[slug]");
 
-  return Response.json({ revalidated: true, now: Date.now() })
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
